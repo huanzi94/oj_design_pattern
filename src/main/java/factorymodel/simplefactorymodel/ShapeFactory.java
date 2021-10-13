19c2c87e35923a93412ce0592970f8a5eca8ef72
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-13 09:34:59 +0800

    简单工厂模式提交

---
+++ src/main/java/factorymodel/simplefactorymodel/ShapeFactory.java
@@ -0,0 +1,23 @@
+package factorymodel.simplefactorymodel;
+
+/**
+ * 画图的工厂类，根据传入的类型，创建不同的图形对象
+ */
+public class ShapeFactory {
+
+    /**
+     * 根据传入的类型，创建不同的图形对象
+     * @param shapeType 类型
+     * @return 具体的图形
+     */
+    public Shape getShape(String shapeType) {
+
+        if (shapeType.equals("Circle")) {
+            return new CircleShape();
+        } else if (shapeType.equals("Triangle")) {
+            return new TriangleShape();
+        } else {
+            return null;
+        }
+    }
+}
