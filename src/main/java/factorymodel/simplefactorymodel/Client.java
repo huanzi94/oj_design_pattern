545f73482c126891de327cf4b80cff3bdceee567
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-13 09:39:23 +0800

    简单工厂模式提交

---
+++ src/main/java/factorymodel/simplefactorymodel/Client.java
@@ -0,0 +1,13 @@
+package factorymodel.simplefactorymodel;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        Shape circle = shapeFactory.getShape("Circle");
+        circle.drawShape();
+
+        Shape triangle = shapeFactory.getShape("Triangle");
+        triangle.drawShape();
+    }
+}
