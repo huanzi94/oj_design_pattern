f934ab3f4a63716635163d74c4b8ea5c448855c0
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-13 09:23:08 +0800

    抽象工厂模式提交

---
+++ src/main/java/factorymodel/abstractfactorymodel/TriangleShape.java
@@ -0,0 +1,12 @@
+package factorymodel.abstrctfactorymodel;
+
+/**
+ * 图形接口的实现，定义了具体的三角形
+ */
+public class TriangleShape implements Shape {
+
+    @Override
+    public void drawShape() {
+        System.out.println("画了一个三角形~");
+    }
+}
