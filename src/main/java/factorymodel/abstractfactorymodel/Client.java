9170993c6c659ca8deff5adbd884e1bf518d508b
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-13 09:27:16 +0800

    抽象工厂模式提交

---
+++ src/main/java/factorymodel/abstractfactorymodel/Client.java
@@ -0,0 +1,16 @@
+package factorymodel.abstrctfactorymodel;
+
+public class Client {
+
+    public static void main(String[] args) {
+        AbstractShapeFactory shapeFactory = GenerateFactory.getFactory("Shape");
+        Shape circle = shapeFactory.getShape("Circle");
+        circle.drawShape();
+        Shape triangle = shapeFactory.getShape("Triangle");
+        triangle.drawShape();
+
+        AbstractShapeFactory otherShapeFactory = GenerateFactory.getFactory("Other");
+        Shape other = otherShapeFactory.getOther("Other");
+        other.drawShape();
+    }
+}
