c1e6473f856525ba09f6b8f0471ad3d037c3d5af
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-22 16:05:26 +0800

    建造者模型提交

---
+++ src/main/java/buildermodel/Pepsi.java
@@ -0,0 +1,16 @@
+package buildermodel;
+
+/**
+ * 百事可乐
+ */
+public class Pepsi extends Drink{
+    @Override
+    public String name() {
+        return "百事可乐";
+    }
+
+    @Override
+    public String material() {
+        return "糖";
+    }
+}
