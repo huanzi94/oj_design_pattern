2128c91b1639a2c8deae0341297f36051793cbb3
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-22 16:11:36 +0800

    命令模型提交

---
+++ src/main/java/commandmodel/TVOnCommand.java
@@ -0,0 +1,23 @@
+package commandmode;
+
+/**
+ * 电灯开机
+ */
+public class TVOnCommand implements Command {
+
+    private final TVReceiver tvReceiver;
+
+    public TVOnCommand(TVReceiver tvReceiver) {
+        this.tvReceiver = tvReceiver;
+    }
+
+    @Override
+    public void execute() {
+        tvReceiver.on();
+    }
+
+    @Override
+    public void undo() {
+        tvReceiver.off();
+    }
+}
