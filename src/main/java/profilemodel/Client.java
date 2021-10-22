d7cfefb23a80241cc019470ac9b2825a887546b5
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-22 16:33:41 +0800

    模板模式提交

---
+++ src/main/java/profilemodel/Client.java
@@ -0,0 +1,11 @@
+package profilemode;
+
+public class Client {
+    public static void main(String[] args) {
+        AbstractBarbecue roastMeat = new RoastMeat();
+        roastMeat.making();
+        System.out.println("++++++++++++++++++++++++++++");
+        AbstractBarbecue roastedSweetPotatoes = new RoastedSweetPotatoes();
+        roastedSweetPotatoes.making();
+    }
+}
