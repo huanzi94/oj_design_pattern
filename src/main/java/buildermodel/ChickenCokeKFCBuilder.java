56e7e2166b6e07984cd21101691889f1a42c79e8
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-22 15:57:14 +0800

    建造者模型提交

---
+++ src/main/java/buildermodel/ChickenCokeKFCBuilder.java
@@ -0,0 +1,31 @@
+package buildermodel;
+
+/**
+ * 鸡肉可口可乐套餐建造者
+ */
+public class ChickenCokeKFCBuilder extends KFCBuilder{
+
+    /**
+     * 鸡肉可口可乐套餐建造者
+     * @param hamburg 具体的汉堡。鸡肉汉堡
+     * @param drink 具体的饮料。可口可乐
+     */
+    public ChickenCokeKFCBuilder(Hamburg hamburg, Drink drink) {
+        super(hamburg, drink);
+    }
+
+    @Override
+    public void makingBurgers() {
+        System.out.println("制作鸡腿汉堡");
+    }
+
+    @Override
+    public void makingDrink() {
+        System.out.println("制作可口可乐");
+    }
+
+    @Override
+    public void packaging() {
+        System.out.println("打包鸡腿汉堡+可口可乐套餐");
+    }
+}
