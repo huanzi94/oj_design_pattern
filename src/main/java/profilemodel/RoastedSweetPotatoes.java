f09681a9225031a9c333255f4021c22b5480224f
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-22 16:35:33 +0800

    模板模式提交

---
+++ src/main/java/profilemodel/RoastedSweetPotatoes.java
@@ -0,0 +1,27 @@
+package profilemode;
+
+/**
+ * 烤红薯
+ */
+public class RoastedSweetPotatoes extends AbstractBarbecue{
+
+    @Override
+    public void stoker() {
+        System.out.println("用煤炭生火");
+    }
+
+    @Override
+    public void prepare() {
+        System.out.println("准备红薯");
+    }
+
+    @Override
+    public void barbecue() {
+        System.out.println("烧烤红薯");
+    }
+
+    @Override
+    public void taking() {
+        System.out.println("红薯烤好了，可以吃了");
+    }
+}
