09bb7947b27b196829104d351056b9e19de37940
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-22 16:10:44 +0800

    命令模型提交

---
+++ src/main/java/commandmodel/TVOffCommand.java
@@ -0,0 +1,23 @@
+package commandmode;
+
+/**
+ * 电灯关机
+ */
+public class TVOffCommand implements Command {
+
+    private final TVReceiver tvReceiver;
+
+    public TVOffCommand(TVReceiver tvReceiver) {
+        this.tvReceiver = tvReceiver;
+    }
+
+    @Override
+    public void execute() {
+        tvReceiver.off();
+    }
+
+    @Override
+    public void undo() {
+        tvReceiver.on();
+    }
+}
