c22b791369c854e2c9401b09eef5a51aa029f528
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-10-22 15:58:03 +0800

    建造者模型提交

---
+++ src/main/java/buildermodel/ChickenHamburgers.java
@@ -0,0 +1,17 @@
+package buildermodel;
+
+/**
+ * 鸡肉汉堡
+ */
+public class ChickenHamburgers extends Hamburg{
+
+    @Override
+    public String name() {
+        return "鸡肉汉堡";
+    }
+
+    @Override
+    public String material() {
+        return "鸡肉";
+    }
+}
