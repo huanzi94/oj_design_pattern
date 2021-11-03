b70cc7ed5704de239a004e45919098f9704bffb4
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-11-03 19:43:37 +0800

    策略模式提交

---
+++ src/main/java/strategymodel/HeightStrategy.java
@@ -0,0 +1,12 @@
+package strategymodel;
+
+/**
+ * 身高比较策略
+ */
+public class HeightStrategy implements Strategy{
+
+    @Override
+    public int compare(Cat cat1, Cat cat2) {
+        return cat1.getHeight() > cat2.getHeight() ? 1 : 0;
+    }
+}
