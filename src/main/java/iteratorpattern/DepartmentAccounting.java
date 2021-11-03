18ed765c70aab5d345392b23ad5d82fa069d13e6
huanzi94 <31958917+huanzi94@users.noreply.github.com>
2021-11-03 20:14:37 +0800

    迭代器模式

---
+++ src/main/java/iteratorpattern/DepartmentAccounting.java
@@ -0,0 +1,16 @@
+package iteratorpattern;
+
+/**
+ * 会计系
+ */
+public class DepartmentAccounting implements CollegeSeries{
+
+    public String getName() {
+        return "会计系";
+    }
+
+    public String getDesc() {
+        return "会计系";
+    }
+}
+
