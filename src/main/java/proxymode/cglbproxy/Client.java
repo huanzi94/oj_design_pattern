b7679ae06451311ccb304c23599ff13d4abbcb5f
huanzi94 <1612902203@qq.com>
2021-11-06 23:19:58 +0800

    代理模式

--- src/main/java/proxymode/cglbproxy/Client.java
+++ src/main/java/proxymode/cglbproxy/Client.java
@@ -1,15 +1,15 @@
 package proxymode.cglbproxy;
 
 
-import java.lang.reflect.Proxy;
+import net.sf.cglib.proxy.Enhancer;
 
 public class Client {
 
     public static void main(String[] args) {
-        CglibProxy cglibProxy = new CglibProxy();
-        ModelALogin proxyModelALogin = new ProxyModelALogin(new ModelALogin(), "张三");
         System.out.println("--------------------使用CGLIB动态代理模式");
-        proxyModelALogin.;
+        ProxyModelALogin proxyModelALogin = new ProxyModelALogin(new ModelALogin(), "张三");
+        ModelALogin login = (ModelALogin) Enhancer.create(ModelALogin.class, proxyModelALogin);
+        login.login();
     }
 }
 
