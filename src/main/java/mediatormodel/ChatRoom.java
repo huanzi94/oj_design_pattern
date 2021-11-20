05aa04d0792c29c01bfb2f7a35d0349e4da44719
huanzi94 <1612902203@qq.com>
2021-11-20 15:45:43 +0800

    中介者模式提交

--- src/main/java/mediatormodel/ChatRoom.java
+++ src/main/java/mediatormodel/ChatRoom.java
@@ -1,15 +1,11 @@
 package mediatormodel;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 /**
  * 聊天室，中介者，所有用户发送过来的消息，都由它处理
  */
 public class ChatRoom {
-
-    List<User> users = new ArrayList<>();
 
     /**
      * 处理消息
@@ -18,9 +14,5 @@ public class ChatRoom {
      */
     public static void showMessage(User user) {
         System.out.println(new Date() + "--" + user.getName() + "--" + user.getMessage());
-    }
-
-    public void addUser(User user) {
-        users.add(user);
     }
 }
--- src/main/java/mediatormodel/User.java
+++ src/main/java/mediatormodel/User.java
@@ -18,16 +18,8 @@ public class User {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     /**
